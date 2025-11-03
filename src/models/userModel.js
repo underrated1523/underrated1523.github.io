79c24930d1294c3707f087308042c1a1c792dbd0
underrated1523 <underrated1523@gmail.com>
2025-11-03 20:46:54 +0900

    Delete src directory

--- src/models/userModel.js
+++
@@ -1,9 +0,0 @@
-export default class UserModel {
-  constructor() {
-    this.userId = "";
-    this.password = "";
-    this.labels = [];
-    this.createdAt = new Date();
-    this.updatedAt = new Date();
-  }
-}
